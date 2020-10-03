1489c9f3a96815c30edce00aedd3d45060098309
Niteshyadav585 <36307599+Niteshyadav585@users.noreply.github.com>
2020-10-03 13:20:10 +0530

    Update duplicate.cpp

--- array_examples/duplicate.cpp
+++ array_examples/duplicate.cpp
@@ -1,28 +1,22 @@
 #include<iostream>
 using namespace std;
- int main()
- {
-     int n,arr[n],m;
+int main(){
+	int n,arr[n],m;
 	cout<<"enter size of array";
 	cin>>n;
 	cout<<"enter element in array";
-	for(int i=0;i<n;i++)
-	{
-		cin>>arr[i];
+
+	for(int i=0;i<n;i++){
+	  cin>>arr[i];
 	}   
 
-cout<<"duplicates elements in given array";
-	for(int i=0;i<n;i++)
-	{
-		for(int j=i+1;j<n;j++)
-		{
-			if(arr[i]==arr[j])
-			{
-              cout<<arr[i];
+	cout<<"Duplicates elements in given array";
+	
+	for(int i=0;i<n;i++){
+		for(int j=i+1;j<n;j++){
+			if(arr[i]==arr[j]){
+	      			cout<<arr[i];
 			}
 		}
 	}
-	
-	
-
  }
